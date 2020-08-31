0dbeee19f1aeca0c4f7af652e7981fc176baf5c4
jaafar mhamed <jaafarmhamed47@gmail.com>
2020-08-31 02:41:06 +0100

    apropos

---
+++ a propos de nous/js/apropos.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function() {
+  
+    var progressBar = document.querySelectorAll(".progress-bar");
+    var time = 1500;
+    
+  
+    progressBar.forEach(function(i) {
+      let label = i.children[0];
+      let line = i.children[1];
+      let count = 0;
+      let dataCount = label.getAttribute("data-count");
+      let lineCount = line.children[0];
+   
+      let runTime = time/dataCount;
+      
+      let animationLineCount = setInterval(function(){
+        if(count < dataCount){
+          count++;
+          label.innerHTML = count + '%';
+          lineCount.style.width = count + '%';
+        }
+      },runTime);
+    });
+  });
+  
